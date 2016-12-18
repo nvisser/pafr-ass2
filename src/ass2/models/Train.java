18cbf4d68d6cfc65868ef0e1f36b0ad95043befb
Casper <casper.reukers@student.hu.nl>
2016-12-18 15:36:35 +0100

    Train en Trainstation :)

--- src/ass2/models/Train.java
+++ src/ass2/models/Train.java
@@ -1,6 +1,21 @@
 package ass2.models;
 
+import java.util.ArrayList;
+
 public class Train
 {
+    private String id;
+    private ArrayList<Wagon> wagonList = new ArrayList<Wagon>();
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void addWagon(Wagon w){
+        wagonList.add(w);
+    }
 }
--- src/ass2/models/Trainstation.java
+++ src/ass2/models/Trainstation.java
@@ -1,6 +1,13 @@
 package ass2.models;
 
+import java.util.ArrayList;
+
 public class Trainstation
 {
+    private ArrayList<Train> trainList = new ArrayList<Train>();
+
+    public void addTrain(Train t){
+        trainList.add(t);
+    }
 
 }

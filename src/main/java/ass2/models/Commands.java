b510fb8a7e00d90fcd7dc6eae69c5e6fd01430de
Niek Visser <niek@bcome.nl>
2016-12-18 16:44:53 +0100

    Add parser

--- src/main/java/ass2/models/Commands.java
+++ src/main/java/ass2/models/Commands.java
@@ -2,4 +2,26 @@ package ass2.models;
 
 public class Commands
 {
+	public static void parse(String input)
+	{
+		String[] args = input.split("\\s+");
+		if (args.length < 3)
+			return;
+		String action = args[0];
+		switch (action)
+		{
+			case "new":
+				String what = args[1];
+				switch (what)
+				{
+					case "train":
+						// todo stuff
+						break;
+					case "wagon":
+						// todo stuff
+						break;
+				}
+				break;
+		}
+	}
 }

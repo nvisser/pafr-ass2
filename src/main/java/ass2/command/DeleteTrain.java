2ae8ef34e66286bc80c980f5d527bd7065a6e44c
Casper <casper.reukers@student.hu.nl>
2016-12-21 15:50:41 +0100

    DeleteTrain

--- src/main/java/ass2/command/DeleteTrain.java
+++ src/main/java/ass2/command/DeleteTrain.java
@@ -1,8 +1,26 @@
 package ass2.command;
 
+import ass2.models.Train;
+import ass2.models.Trainstation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Brian on 19-12-2016 at 15:10.
  */
-public class DeleteTrain
+public class DeleteTrain extends AbstractCommand
 {
+    private static final Pattern pattern = Pattern.compile("delete train ([a-z][a-z0-9]*);");
+
+    @Override
+    protected Pattern getPattern(){
+        return pattern;
+    }
+
+    @Override
+    protected Boolean executeCommand(Matcher matcher){
+
+    }
+
 }

dee8d4c420ee1509399618ea7cd491c651564a98
Niek Visser <niek@bcome.nl>
2016-12-22 13:24:52 +0100

    Remove wagon

--- src/main/java/ass2/command/RemoveWagon.java
+++ src/main/java/ass2/command/RemoveWagon.java
@@ -1,5 +1,19 @@
 package ass2.command;
 
-public class RemoveWagon
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RemoveWagon extends AbstractCommand
 {
+	@Override
+	protected Pattern getPattern()
+	{
+		return null;
+	}
+
+	@Override
+	protected boolean executeCommand(Matcher matcher)
+	{
+		return false;
+	}
 }

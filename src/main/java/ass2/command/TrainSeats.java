0d01cfb2eaa9af61711012c4a87dfa23e6607eee
Casper <casper.reukers@student.hu.nl>
2016-12-22 14:21:49 +0100

    TrainSeats

---
+++ src/main/java/ass2/command/TrainSeats.java
@@ -0,0 +1,30 @@
+package ass2.command;
+
+import ass2.models.Train;
+import ass2.models.Trainstation;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Casper on 22-12-2016.
+ */
+public class TrainSeats extends AbstractCommand {
+
+    private static final Pattern pattern = Pattern.compile("getnumseats train ([a-z][a-z0-9]*);");
+
+    @Override
+    protected Pattern getPattern(){
+        return pattern;
+    }
+
+    @Override
+    public boolean executeCommand(Matcher matcher){
+        ArrayList<Train> a = Trainstation.getInstance().getTrainList();
+        Optional<Train> found = a.stream().filter((t) -> t.getId().equals(matcher.group(1))).findFirst();
+        found.ifPresent(train -> Trainstation.getInstance().get);
+        return true;
+    }
+}
